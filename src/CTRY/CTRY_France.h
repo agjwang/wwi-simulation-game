2f2f93c640d1146dee8796e49d73d2c0366cf58e
Cathy Hua <cathy.hua@gmail.com>
2017-05-20 16:11:58 -0700

    Added header files for France and GreatBritain

---
+++ src/CTRY/CTRY_France.h
@@ -0,0 +1,31 @@
+/*
+ * NAME: CTRY_France.h
+ *
+ * COMMENTS: Country entity (singleton) data model for France
+ */
+
+#ifndef __CTRY_France_h__
+#define __CTRY_France_h__
+
+#include "CTRY_Base.h"
+
+class CTRY_France:public CTRY_Base
+{
+        public:
+            //Accessor method for static instance of class
+            static CTRY_France&         getInstance();
+
+            //Public deconstructor
+                                       ~CTRY_France();
+
+        private:
+            //Private constructor
+                                        CTRY_France();
+                                        CTRY_France(CTRY_France const&);
+            void                        operator=(CTRY_France const&);
+            
+            //static instance of class 
+            static CTRY_France*         classInstance;
+}
+
+#endif
