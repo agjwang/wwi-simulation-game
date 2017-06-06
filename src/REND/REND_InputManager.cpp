09015bc068c4911bd5f41b88e1ab0c5c46a37224
Alex Wang <alexander.gj.wang@gmail.com>
2017-06-06 12:01:21 -0400

    added callback functions to REND_InputManager

--- src/REND/REND_InputManager.cpp
+++ src/REND/REND_InputManager.cpp
@@ -17,16 +17,87 @@ REND_InputManager::~REND_InputManager()
 
 void REND_InputManager::init()
 {
-
+    assert(!windowHandle);
+    glfwSetKeyCallback(windowHandle, keyCallback);
+    glfwSetCursorPosCallback(windowHandle, cursorPosCallback);
+    glfwSetMouseButtonCallback(windowHandle, mouseButtonCallback);
 }
 
 void REND_InputManager::update()
 {
-
 }
 
 void REND_InputManager::cleanup()
 {
 }
 
+void REND_InputManager::keyCallback(GLFWwindow* window, int key, int scancode,
+        int action, int mods)
+{
+    keyboardButton = key;
 
+    switch (button)
+    {
+        case GLFW_MOUSE_BUTTON_LEFT:
+            mouseButton = KEY_LEFT_BUTTON;
+            break;
+        case GLFW_MOUSE_BUTTON_MIDDLE:
+            mouseButton = KEY_MIDDLE_BUTTON;
+            break;
+        case GLFW_MOUSE_BUTTON_RIGHT:
+            mouseButton = KEY_RIGHT_BUTTON;
+            break;
+        default:
+            mouseButton = KEY_NO_BUTTON;
+            break;
+    }
+}
+
+void REND_InputManager::cursorPosCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    // TODO: Conversion factor to screen coordinates might be required
+    mouseXPos = xpos;
+    mouseYPos = ypos;
+}
+
+void REND_InputManager::mouseButtonCallback(GLFWwindow* window, int button,
+        int action, int mods)
+{
+    switch (button)
+    {
+        case GLFW_MOUSE_BUTTON_LEFT:
+            mouseButton = KEY_LEFT_BUTTON;
+            break;
+        case GLFW_MOUSE_BUTTON_MIDDLE:
+            mouseButton = KEY_MIDDLE_BUTTON;
+            break;
+        case GLFW_MOUSE_BUTTON_RIGHT:
+            mouseButton = KEY_RIGHT_BUTTON;
+            break;
+        default:
+            mouseButton = KEY_NO_BUTTON;
+            break;
+    }
+
+    switch (action)
+    {
+        case GLFW_PRESS:
+            mouseAction = MOUSE_PRESS;
+            break;
+        case GLFW_REPEAT:
+            mouseAction = MOUSE_HELD;
+            break;
+        case GLFW_RELEASE:
+            mouseAction = MOUSE_RELEASE;
+            break;
+        default:
+            mouseAction = MOUSE_NO_ACTION;
+            break;
+    }
+}
+
+void REND_InputManager::getMouseCoords(int &xpos, int &ypos)
+{
+    xpos = mouseXPos;
+    ypos = mouseYPos;
+}
--- src/REND/REND_InputManager.h
+++ src/REND/REND_InputManager.h
@@ -16,22 +16,70 @@ class REND_InputManager
 
     public:
 
+        enum MouseButton
+        {
+            MOUSE_NO_BUTTON,
+            MOUSE_LEFT_BUTTON,
+            MOUSE_MIDDLE_BUTTON,
+            MOUSE_RIGHT_BUTTON,
+            NUM_MOUSE_BUTTONS   // Cannot set as this, used only for num of buttons
+        };
+
+        enum KeyAction
+        {
+            KEY_NO_ACTION = 0,
+            KEY_PRESS,
+            KEY_HELD,
+            KEY_RELEASE,
+            NUM_KEY_ACTIONS   // Cannot set as this, used only for num of actions
+        };
+
         // Class constructor and destructor
                                  InputManager(Window window);
         virtual                 ~InputManager();
 
         // Methods to set up and update input manager
+        // Set up and maintain callback functions
         void                     init();
         
         void                     update();
 
         void                     cleanup();
 
+
+        void                     getMouseCoords(int &xpos, int &ypos);
+
+        MouseButton              getMouseButton();
+                                { return mouseButton; }
+        KeyAction                getMouseAction()
+                                { return mouseAction; }
+
+        int                      getKeyboardButton()
+                                { return keyboardButton; }
+        KeyAction                getKeyboardAction()
+                                { return keyboardAction; }
     protected:
 
+
     private:
+
+        void                     keyCallback(GLFWwindow* window, int key,
+                                    int scancode, int action, int mods);
+        void                     cursorPosCallback(GLFWwindow* window,
+                                    double xpos, double ypos);
+        void                     mouseButtonCallback(GLFWwindow* window, 
+                                    int button, int action, int mods);
         
         GLFWwindow              *windowHandle;
+        
+        MouseButton              mouseButton;
+        KeyAction                mouseAction;
+
+        double                   mouseXPos;
+        double                   mouseYPos;
+        
+        int                      keyboardButton;
+        KeyAction                keyboardAction;
 };
 
 #endif
